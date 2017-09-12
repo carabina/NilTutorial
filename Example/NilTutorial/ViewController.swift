56b05e3cab0780b8ae1154674ac7779df92fecf0
Tanasak <nilc.nolan@gmail.com>
2017-09-12 10:35:09 +0700

    Init Commit

--- Example/NilTutorial/ViewController.swift
+++ Example/NilTutorial/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import NilTutorial
+
 
 class ViewController: UIViewController {
 
@@ -15,10 +17,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let testVC = NilTutorial()
+        
+        
+    }
+    
 }
 
---
+++ NilTutorial/Classes/NilTutorial.swift
@@ -0,0 +1,31 @@
+//
+//  NilTutorial.swift
+//  Pods
+//
+//  Created by Tanasak Ngerniam on 9/12/2560 BE.
+//
+//
+
+import UIKit
+
+public class NilTutorial: UIViewController {
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
